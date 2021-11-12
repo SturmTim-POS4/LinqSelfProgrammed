ae4d403eefa888266fdf1be72e44b72c39dc80a1
SturmTim <Sturm.Tim@sus.htl-grieskirchen.at>
2021-11-12 12:42:44 +0100

    Finished

--- LinqSelfProgrammed/LinqQueries.cs
+++ LinqSelfProgrammed/LinqQueries.cs
@@ -23,183 +23,137 @@ namespace LinqSelfProgrammed
     #region ---------------------------------------------------- Filters
     public IEnumerable<int> IntegersGT50()
     {
-      var result = new List<int>();
-      foreach (var variable in integers)
-      {
-        if(variable > 50) result.Add(variable);
-      }
-      return result;
-      //return integers.Where(x => x > 50);
+      
+      return integers.Where(x => x > 50);
     }
     public IEnumerable<string> StringsWithLength4()
     {
-      var result = new List<string>();
-      foreach (var variable in strings)
-      {
-        if(variable.Length == 4) result.Add(variable);
-      }
-      return result;
-      //return strings.Where(x => x.Length == 4);
+      return strings.Where(x => x.Length == 4);
     }
     public IEnumerable<Person> PersonsWithSallaryGT2500()
     {
-      var result = new List<Person>();
-      foreach (var variable in persons)
-      {
-        if(variable.Salary > 2500) result.Add(variable);
-      }
-      return result;
-      //return persons.Where(x => x.Salary > 2500);
+      return persons.Where(x => x.Salary > 2500);
     }
     public IEnumerable<double> DoublesTake4()
     {
-      var result = new List<double>();
-      for (int i = 0; i < doubles.Count && i < 4; i++)
-      {
-        result.Add(doubles[i]);
-      }
-      return result;
-      //return doubles.Take(4);
+      return doubles.Take(4);
     }
     public IEnumerable<Person> PersonsSkip2()
     {
-      return new List<Person>();
-      //return persons.Skip(2);
+      return persons.Skip(2);
     }
     public IEnumerable<int> IntegersTakeWhileLT100()
     {
-      return new List<int>();
-      //return integers.TakeWhile(x => x < 100);
+      return integers.TakeWhile(x => x < 100);
     }
     public IEnumerable<string> StringsSkipWhileX1EQa()
     {
-      return new List<string>();
-      //return strings.SkipWhile(x => x[1] == 'a');
+      return strings.SkipWhile(x => x[1] == 'a');
     }
     public IEnumerable<int> IntegersDistinct()
     {
-      return new List<int>();
-      //return integers.Distinct();
+      return integers.Distinct();
     }
     #endregion
 
     #region ---------------------------------------------------- Projection
     public IEnumerable<int> DoublesToInt()
     {
-      return new List<int>();
-      //return doubles.Select(x => (int)x);
+      return doubles.Select(x => (int)x);
     }
     public IEnumerable<string> StringsToUpper()
     {
-      return new List<string>();
-      //return strings.Select(x => x.ToUpper());
+      return strings.Select(x => x.ToUpper());
     }
     public IEnumerable<string> PersonsToLastnameSallary()
     {
-      return new List<string>();
-      //return persons.Select(x => $"{x.Lastname} --> {x.Salary}");
+      return persons.Select(x => $"{x.Lastname} --> {x.Salary}");
     }
     public IEnumerable<object> PersonsToAnonymous()
     {
-      return new List<object>();
-      //return persons.Select(x => new { Name = $"{x.Firstname} {x.Lastname}" });
+      return persons.Select(x => new { Name = $"{x.Firstname} {x.Lastname}" });
     }
     #endregion
 
     #region ---------------------------------------------------- First/Last
     public double FirstDouble()
     {
-      return -666;
-      //return doubles.First();
+      return doubles.First();
     }
     public Person FirstPersonAgeLT60()
     {
-      return null;
-      //return persons.First(x => x.Age < 60);
+      return persons.First(x => x.Age < 60);
     }
     public double LastDouble()
     {
-      return -666;
-      //return doubles.Last();
+      return doubles.Last();
     }
     public int FirstOrDefaultIntGT1000()
     {
-      return -666;
-      //return integers.FirstOrDefault(x => x > 1000);
+      return integers.FirstOrDefault(x => x > 1000);
     }
     #endregion
 
     #region ---------------------------------------------------- Aggregation
     public int AverageInt()
     {
-      return -666;
-      //return integers.Average();
+      return integers.Average();
     }
     public double AverageDouble()
     {
-      return -666;
-      //return doubles.Average();
+      return doubles.Average();
     }
     public double AveragePersonSalary()
     {
-      return -666;
-      //return persons.Average(x => x.Salary);
+      return persons.Average(x => x.Salary);
     }
     public int CountPersonsAgeGT60()
     {
-      return -666;
-      //return persons.Count(x => x.Age > 60);
+      return persons.Count(x => x.Age > 60);
     }
     public int CountPersonsWithLastnameLengthGT5()
     {
-      return -666;
-      //return persons.Count(x => x.Lastname.Length > 5);
+      return persons.Count(x => x.Lastname.Length > 5);
     }
     #endregion
 
     #region ---------------------------------------------------- Sorting
     public IEnumerable<int> IntsSorted()
     {
-      return new List<int>();
-      //return integers.OrderBy(x => x);
+      return integers.OrderBy(x => x);
     }
     public IEnumerable<double> DoublesSortedDescending()
     {
-      return new List<double>();
-      //return doubles.OrderByDescending(x => x);
+      return doubles.OrderByDescending(x => x);
     }
     public IEnumerable<Person> PersonsSortedDescendingBySalary()
     {
-      return new List<Person>();
-      //return persons.OrderByDescending(x => x.Salary);
+      return persons.OrderByDescending(x => x.Salary);
     }
     public IEnumerable<string> PersonFirstnamesSortedDescendingBySalary()
     {
-      return new List<string>();
-      //return persons.OrderByDescending(x => x.Salary).Select(x => x.Firstname);
+      return persons.OrderByDescending(x => x.Salary).Select(x => x.Firstname);
     }
     #endregion
 
     #region ---------------------------------------------------- Combined
     public IEnumerable<string> CombinedA()
     {
-      return new List<string>();
-      //return persons
-      //.Where(x => x.Age < 70)
-      //.Skip(1)
-      //.Take(2)
-      //.Select(x => $"{x.Firstname} {x.Lastname}");
+      return persons
+      .Where(x => x.Age < 70)
+      .Skip(1)
+      .Take(2)
+      .Select(x => $"{x.Firstname} {x.Lastname}");
     }
     public IEnumerable<double> CombinedB()
     {
       CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
-      return new List<double>();
-      //return doubles
-      //.Select(x => $"{x}".Split('.'))
-      //.Where(x => x[1].Length > 2)
-      //.Where(x => int.Parse(x[0]) % 2 == 0)
-      //.Select(x => string.Join(".", x))
-      //.Select(x => double.Parse(x));
+      return doubles
+      .Select(x => $"{x}".Split('.'))
+      .Where(x => x[1].Length > 2)
+      .Where(x => int.Parse(x[0]) % 2 == 0)
+      .Select(x => string.Join(".", x))
+      .Select(x => double.Parse(x));
     }
     #endregion
 
--- MyLinqLib/MyLinqExtensions.cs
+++ MyLinqLib/MyLinqExtensions.cs
@@ -5,7 +5,373 @@ namespace MyLinqLib
 {
   public static class MyLinqExtensions
   {
+      #region SelectElement
+      
+      public static T First<T>(this IEnumerable<T> list, Func<T, bool> function = null)
+      {
+          foreach (var variable in list)
+          {
+              if (function == null || function(variable)) return variable;
+          }
 
+          return default;
+      }
+      
+      public static T FirstOrDefault<T>(this IEnumerable<T> list, Func<T, bool> function = null)
+      {
+          foreach (var variable in list)
+          {
+              if (function != null && function(variable)) return variable;
+              if (function == null) return variable;
+          }
+          return default;
+      }
+      
+      public static T Last<T>(this IEnumerable<T> list, Func<T, bool> function = null)
+      {
+          return list.Reverse(function).First();
+      }
+      
+      public static T LastOrDefault<T>(this IEnumerable<T> list, Func<T, bool> function = null)
+      {
+          return list.Reverse(function).FirstOrDefault(function);
+      }
+      
+      public static T Single<T>(this IEnumerable<T> list, Func<T, bool> function = null)
+      {
+          var result = new List<T>();
+          foreach (var variable in list)
+          {
+              if(function == null || function(variable)) result.Add(variable);
+          }
 
+          if (result.Count() > 1) throw InvalidOperationException;
+          return result[0];
+      }
+
+      public static Exception InvalidOperationException { get; set; }
+
+      public static T SingleOrDefault<T>(this IEnumerable<T> list, Func<T, bool> function = null)
+      {
+          var result = new List<T>();
+          foreach (var variable in list)
+          {
+              if(function == null || function(variable)) result.Add(variable);
+          }
+
+          if (result.Count() > 1) throw InvalidOperationException;
+          if (result.Count() == 1) return result[0];
+          return default;
+      }
+      
+      public static T ElementAt<T>(this IEnumerable<T> list, int index)
+      {
+          T result = default;
+          int count = 0;
+          foreach (var variable in list)
+          {
+              if (count == index) result = variable;
+              count++;
+          }
+          return result;
+      }
+      
+      public static T ElementAtOrDefault<T>(this IEnumerable<T> list, int index)
+      {
+          return list.ElementAt(index);
+      }
+      #endregion
+
+      #region Aggregation
+      
+      public static int Average(this IEnumerable<int> list)
+      {
+          int sum = 0;
+          foreach (var variable in list)
+          {
+              sum += variable;
+          }
+
+          return sum/list.Count();
+      }
+      
+      public static double Average(this IEnumerable<double> list)
+      {
+          double sum = 0;
+          foreach (var variable in list)
+          {
+              sum += variable;
+          }
+          return sum/list.Count();
+      }
+      
+      public static int Average<T>(this IEnumerable<T> list, Func<T,int> function)
+      {
+          int sum = 0;
+          foreach (var variable in list)
+          {
+              sum += function(variable);
+          }
+
+          return sum/list.Count();
+      }
+      
+      public static double Average<T>(this IEnumerable<T> list, Func<T,double> function)
+      {
+          double sum = 0;
+          foreach (var variable in list)
+          {
+              sum += function(variable);
+          }
+          return sum/list.Count();
+      }
+      
+      public static int Count<T>(this IEnumerable<T> list, Func<T, bool> function = null)
+      {
+          int count = 0;
+          foreach (var variable in list)
+          {
+              if (function == null || function(variable)) count++;
+          }
+
+          return count;
+      }
+      
+      public static int Sum(this IEnumerable<int> list)
+      {
+          int sum = 0;
+          foreach (var variable in list)
+          {
+              sum += variable;
+          }
+
+          return sum;
+      }
+      
+      public static double Sum(this IEnumerable<double> list)
+      {
+          double sum = 0;
+          foreach (var variable in list)
+          {
+              sum += variable;
+          }
+          return sum;
+      }
+      
+      public static int Sum<T>(this IEnumerable<T> list, Func<T, int> function)
+      {
+          int sum = 0;
+          foreach (var variable in list)
+          {
+              sum += function(variable);
+          }
+
+          return sum;
+      }
+      
+      public static double Sum<T>(this IEnumerable<T> list, Func<T, double> function)
+      {
+          double sum = 0;
+          foreach (var variable in list)
+          {
+              sum += function(variable);
+          }
+          return sum;
+      }
+      
+      public static int Max(this IEnumerable<int> list)
+      {
+          int max = Int32.MinValue;
+          foreach (var variable in list)
+          {
+              if (max < variable) max = variable;
+          }
+          return max;
+      }
+      
+      public static double Max(this IEnumerable<double> list)
+      {
+          double max = Double.MinValue;
+          foreach (var variable in list)
+          {
+              if (max < variable) max = variable;
+          }
+          return max;
+      }
+      
+      public static int Max<T>(this IEnumerable<T> list, Func<T, int> function)
+      {
+          int max = Int32.MinValue;
+          foreach (var variable in list)
+          {
+              if (max < function(variable)) max = function(variable);
+          }
+          return max;
+      }
+      
+      public static double Max<T>(this IEnumerable<T> list, Func<T, double> function)
+      {
+          double max = Double.MinValue;
+          foreach (var variable in list)
+          {
+              if (max < function(variable)) max = function(variable);
+          }
+          return max;
+      }
+      
+      public static int Min(this IEnumerable<int> list)
+      {
+          int min = Int32.MaxValue;
+          foreach (var variable in list)
+          {
+              if (min > variable) min = variable;
+          }
+          return min;
+      }
+      
+      public static double Min(this IEnumerable<double> list)
+      {
+          double min = Double.MaxValue;
+          foreach (var variable in list)
+          {
+              if (min > variable) min = variable;
+          }
+          return min;
+      }
+      
+      public static int Min<T>(this IEnumerable<T> list, Func<T, int> function)
+      {
+          int min = Int32.MaxValue;
+          foreach (var variable in list)
+          {
+              if (min > function(variable)) min = function(variable);
+          }
+          return min;
+      }
+      
+      public static double Min<T>(this IEnumerable<T> list, Func<T, double> function)
+      {
+          double min = Double.MaxValue;
+          foreach (var variable in list)
+          {
+              if (min > function(variable)) min = function(variable);
+          }
+          return min;
+      }
+      #endregion
+
+      #region Filters
+
+      public static List<T> Where<T>(this IEnumerable<T> list, Func<T, bool> function)
+      {
+          var result = new List<T>();
+          foreach (var variable in list)
+          {
+              if(function(variable)) result.Add(variable);
+          }
+
+          return result;
+      }
+      
+      public static List<T> Distinct<T>(this IEnumerable<T> list)
+      {
+          var result = new List<T>();
+          foreach (var variable in list)
+          {
+              if(!result.Contains(variable)) result.Add(variable);
+          }
+
+          return result;
+      }
+      
+      public static List<T> Take<T>(this IEnumerable<T> list, int number)
+      {
+          var result = new List<T>();
+          int count = 0;
+          foreach (var variable in list)
+          {
+              if (count == number) break;
+              result.Add(variable);
+              count++;
+          }
+
+          return result;
+      }
+      
+      public static List<T> TakeWhile<T>(this IEnumerable<T> list, Func<T, bool> function)
+      {
+          var result = new List<T>();
+          foreach (var variable in list)
+          {
+              if (function(variable)) result.Add(variable);
+              else break;
+          }
+          return result;
+      }
+      
+      public static List<T> Skip<T>(this IEnumerable<T> list, int number)
+      {
+          var result = new List<T>();
+          int count = 0;
+          foreach (var variable in list)
+          {
+              if (count > number) result.Add(variable);
+              count++;
+          }
+          return result;
+      }
+      
+      public static List<T> SkipWhile<T>(this IEnumerable<T> list, Func<T, bool> function)
+      {
+          var result = new List<T>();
+          foreach (var variable in list)
+          {
+              if (!function(variable)) result.Add(variable);
+          }
+          return result;
+      }
+      
+      #endregion
+
+      #region Projizieren
+      
+      public static List<TResult> Select<TSource,TResult>(this IEnumerable<TSource> list, Func<TSource, TResult> function)
+      {
+          var result = new List<TResult>();
+          foreach (var variable in list)
+          {
+              result.Add(function(variable));
+          }
+
+          return result;
+      }
+      
+      #endregion
+      
+      public static List<T> OrderBy<T,TKey>(this List<T> list, Func<T, TKey> function)
+      {
+          list.Sort((a,b) => Comparer<TKey>.Default.Compare(function(a),function(b)));
+          return list;
+      }
+      
+      public static List<T> OrderByDescending<T,TKey>(this IEnumerable<T> list, Func<T, TKey> function)
+      {
+          var result = new List<T>();
+          result.AddRange(list);
+          result.Reverse();
+          return result;
+      }
+      
+      public static List<T> Reverse<T>(this IEnumerable<T> list, Func<T, bool> function = null)
+      {
+          var result = new List<T>();
+          foreach (var variable in list)
+          {
+              result.Add(variable);
+          }
+
+          result.Reverse();
+          return result;
+      }
   }
 }
